a3e0a5a54539392a860e89f2d4f2f39ae37750de
bellorini <edmar.bellorini@gmail.com>
2022-03-02 11:21:35 -0300

    correção do link para permissões de arquivos e correção de comentários em a06e01.asm

--- Etapa 01/Aula 06 - Chamadas de Sistemas/codes/a06e01.asm
+++ Etapa 01/Aula 06 - Chamadas de Sistemas/codes/a06e01.asm
@@ -25,7 +25,7 @@ section .text
 
 _start:
     ; ssize_t write(int fd , const void *buf, size_t count);
-    ; eax     write(int ebx, const void *ecx, size_t edx  );
+    ; eax     write(int edi, const void *rsi, size_t edx  );
     mov rax, 1
     mov rdi, 1  ; std_file
     lea rsi, [strOla]
@@ -36,7 +36,7 @@ leitura:
     mov dword [strLidaL], maxChars ; %define é constantes!
 
     ; ssize_t read(int fd , const void *buf, size_t count);
-    ; eax     read(int ebx, const void *ecx, size_t edx  );
+    ; eax     read(int edi, const void *rsi, size_t edx  );
     mov rax, 0  ; READ
     mov rdi, 1
     lea rsi, [strLida]
@@ -65,8 +65,8 @@ resposta:
     syscall
 
 fim:
-    ; void _exit(int status);
-    ; void _exit(int ebx   );
+    ; void _exit(int status );
+    ; void _exit(int edi/rdi);
     mov rax, 60
     mov rdi, 0
     syscall
